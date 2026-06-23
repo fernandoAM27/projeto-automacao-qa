bb7e0499c6bd59c8b731465717fb4fde68401af2
Fernando Arrais <fernandoarraismachado@gmail.com>
2026-06-22 22:42:02 -0300

    Primeiro commit de automação com Playwright

---
+++ tests/lead.spec.js
@@ -0,0 +1,10 @@
+const { test, expect } = require('@playwright/test');
+
+test('deve cadastrar um lead na fila de espera', async ({ page }) => {
+    await page.goto('http://localhost:3000');
+
+
+    await page.getByRole('button', { name: /Aperte o play/ }).click()
+
+    await page.waitForTimeout(10000)
+});
